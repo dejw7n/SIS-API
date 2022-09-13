2111700becb4907f21ee454da318067f1dac690b
Dejw7n <dejw7n@gmail.com>
2022-09-13 17:25:33 +0200

    Add asyncQuery in db

--- src/db.js
+++ src/db.js
@@ -1,10 +1,22 @@
 const mysql = require("mysql");
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
 	host: "91.224.90.56",
 	user: "u10_0mYakItBYz",
 	password: "8OsjAE.O4H^UKtEDDzlQDUc^",
 	database: "s10_spsul_sis",
 });
 
-module.exports = db;
+function asyncQuery(query, params) {
+	return new Promise((resolve, reject) => {
+		pool.query(query, params, (err, result) => {
+			if (err) return reject(err);
+			resolve(result);
+		});
+	});
+}
+
+module.exports = {
+	pool,
+	asyncQuery,
+};
