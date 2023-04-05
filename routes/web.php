f3b48e1b24ee14f71da9c8cca7dbdb632608595c
dejw7n <dejw7n@gmail.com>
2023-04-05 18:30:12 +0200

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->group(['prefix' => 'file'], function () use ($router) {
+        $router->get('', ['uses' => 'FileController@showAllFiles']);
+        $router->get('{id}', ['uses' => 'FileController@showOneFile']);
+        $router->post('', ['uses' => 'FileController@create']);
+        $router->delete('/{id}', ['uses' => 'FileController@delete']);
+        $router->put('/{id}', ['uses' => 'FileController@update']);
+    });
+    $router->group(['prefix' => 'post'], function () use ($router) {
+        $router->get('', ['uses' => 'PostController@showAllPosts']);
+        $router->get('{id}', ['uses' => 'PostController@showOnePost']);
+        $router->post('', ['uses' => 'PostController@create']);
+        $router->delete('/{id}', ['uses' => 'PostController@delete']);
+        $router->put('/{id}', ['uses' => 'PostController@update']);
+    });
+    $router->group(['prefix' => 'user'], function () use ($router) {
+        $router->get('', ['uses' => 'UserController@showAllUsers']);
+        $router->get('{id}', ['uses' => 'UserController@showOneUser']);
+        $router->post('', ['uses' => 'UserController@create']);
+        $router->delete('/{id}', ['uses' => 'UserController@delete']);
+        $router->put('/{id}', ['uses' => 'UserController@update']);
+    });
+});
