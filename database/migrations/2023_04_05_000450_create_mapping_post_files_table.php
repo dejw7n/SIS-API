2698ae29ef92bdb3a2aa0d378e69812c52289706
dejw7n <dejw7n@gmail.com>
2023-04-05 16:17:44 +0200

    Add database migrations

---
+++ database/migrations/2023_04_05_000450_create_mapping_post_files_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mapping_post_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained('posts');
+            $table->foreignId('file_id')->constrained('files');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mapping_post_files');
+    }
+};
