176a01e9d8d9014e28461ddff1af0e5826e3f28b
dejw7n <dejw7n@gmail.com>
2023-05-31 00:33:10 +0200

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,14 +19,14 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      *
      * @var string[]
      */
-    protected $fillable = ['name', 'lname', 'email', 'phone', 'role_id', 'center_id'];
+    protected $fillable = ['name', 'lname', 'email', 'password', 'phone', 'role_id', 'center_id'];
 
     /**
      * The attributes excluded from the model's JSON form.
      *
      * @var string[]
      */
-    protected $hidden = ['password'];
+    //protected $hidden = ['password'];
 
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
