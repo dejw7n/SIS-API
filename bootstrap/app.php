88097bdbec9bbcbc8751039947de8963cfb95f83
dejw7n <dejw7n@gmail.com>
2023-09-16 20:53:37 +0200

    Register mail service provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -89,6 +89,8 @@ $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(Chuckrincon\LumenConfigDiscover\DiscoverServiceProvider::class);
 
+$app->register(\Illuminate\Mail\MailServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
